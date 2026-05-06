f86c08ee3d05e1d976ade429722464bc11404ded
Britney Glory Chen <bilbrit123@gmail.com>
2026-05-06 12:04:59 +0800

    delete laravel_sanctum

--- dueday-be/app/Models/User.php
+++ dueday-be/app/Models/User.php
@@ -9,14 +9,13 @@ use Illuminate\Database\Eloquent\Attributes\Hidden;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-use Laravel\Sanctum\HasApiTokens;
 
 #[Fillable(['username', 'nickname', 'name', 'email', 'password', 'nim', 'status', 'language', 'google_access_token', 'google_refresh_token'])]
 #[Hidden(['password'])]
 class User extends Authenticatable
 {
     /** @use HasFactory<UserFactory> */
-    use HasFactory, Notifiable, HasApiTokens;
+    use HasFactory, Notifiable;
 
     /**
      * Get the attributes that should be cast.
