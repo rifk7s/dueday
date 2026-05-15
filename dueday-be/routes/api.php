145abd9d1270b8606c044faf9ba148d08ddd81ff
Britney Glory Chen <bilbrit123@gmail.com>
2026-05-15 18:49:47 +0800

    dashboard selesai

--- dueday-be/routes/api.php
+++ dueday-be/routes/api.php
@@ -9,6 +9,7 @@ use Illuminate\Support\Facades\Route;
 Route::post('/login', [AuthController::class, 'login']);
 
 Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/me', [AuthController::class, 'me']);
     Route::post('/logout', [AuthController::class, 'logout']);
     
     // Tasks
